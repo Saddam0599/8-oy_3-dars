22330e22fa23229065a38585418814db9af4d7cc
Saddam0599 <saddammamatkulov@gmail.com>
2024-10-23 04:28:02 +0500

    yangi first commit

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,25 +1,12 @@
-// import React from 'react'
+import React from 'react'
 
-// const page = () => {
-//     return (
-//         <div>
-//             <h2 className="text-center text-3xl font-bold sm:p-20 font-[family-name:var(--font-geist-sans)]">About</h2>
-//         </div>
-//     )
-// }
-
-// export default page;
-
-
-
-
-export default function AboutPage() {
-  return (
-    <div className="container mx-auto py-8">
-      <h1 className="text-3xl font-bold mb-4">About Us</h1>
-      <p className="text-lg">
-        This is the about page of our website. Here, you can find more information about what we do and our goals.
-      </p>
-    </div>
-  );
+const page = () => {
+    return (
+        <div>
+            <h2 className="text-center text-3xl font-bold sm:p-20 font-[family-name:var(--font-geist-sans)]">About</h2>
+        </div>
+    )
 }
+
+export default page;
+
